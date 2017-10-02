e0545badfbf52d4409c3186c7aad748295260fd9
Zahra Sheikhbahaee <sheikhbahaee@gmail.com>
2017-10-02 20:27:21 +0200

    working code debugged from pointer problem

--- ars_old.pyx
+++ ars_old.pyx
@@ -143,21 +143,22 @@ cdef void initial(int *ns, int *m, double *emax, double* x, double* hx, double*
       rwv[6] = huzmax
       rwv[7] = xlb[0]
       rwv[8] = xub[0]
-      rwv[iscum+1] = 1.0
+      rwv[iscum] = 1.0
       for i from 0 <= i < m[0]:
-         rwv[ix+i+1] = x[i]
-         rwv[ihx+i+1] = hx[i]
-         rwv[ihpx+i+1] = hpx[i]
+         rwv[ix+i] = x[i]
+         rwv[ihx+i] = hx[i]
+         rwv[ihpx+i] = hpx[i]
       #create lower and upper hulls
       i = 0
       while (i < (m[0]-1)):
-            update(&iwv[3], &iwv[0], &iwv[1], &iwv[iipt+1], &rwv[iscum+1], &rwv[4],
-                    &rwv[ix+1], &rwv[ihx+1], &rwv[ihpx+1], &rwv[iz+1],
+            update(&iwv[3], &iwv[0], &iwv[1], &iwv[iipt+1], &rwv[iscum], &rwv[4],
+                    &rwv[ix], &rwv[ihx], &rwv[ihpx], &rwv[iz+1],
                     &rwv[ihuz+1], &rwv[6], &rwv[2], lb, &rwv[7], &rwv[0], ub,
                     &rwv[8], &rwv[1], ifault, &rwv[3], &rwv[5])
             i = iwv[3]
             if (ifault[0] != 0):
                return
+      print "UPDATE FINISHED!!!"
       #test for wrong starting points
       if ((not lb[0]) and (hpx[iwv[0]] < eps)):
          ifault[0] = 3
@@ -229,6 +230,7 @@ cdef void spl1(int *ns, int *n, int *ilow, int *ihigh, int* ipt, double* scum,
      while ((not sampld) and (attempts < max_attempt)):
          
          u2 = c_libc_random()/rm
+         print "u2:",u2
          #test for zero random number
          if (u2 == 0.0):
             ifault[0] = 6
@@ -241,7 +243,6 @@ cdef void spl1(int *ns, int *n, int *ilow, int *ihigh, int* ipt, double* scum,
          alu1 = log(u1)
          # compute alhu: upper hull at point u1
          alhu = hpx[i]*(beta[0]-x[i])+hx[i]-huzmax[0]
-         
          if ((beta[0] > x[ilow[0]]) and (beta[0] < x[ihigh[0]])):
             # compute alhl: value of the lower hull at point u1
             if (beta[0] > x[i]):
@@ -252,15 +253,17 @@ cdef void spl1(int *ns, int *n, int *ilow, int *ihigh, int* ipt, double* scum,
             if ((alhl-alhu) > alu1):
                
                sampld = True
+         print "update"
          #if not sampled evaluate the function, do the rejection test and update
          if (not sampld):
-            n1    = n[0]+1
+            n1 = n[0]+1
             x[n1] = beta[0]
             #defining log of the distribution and its derivitive
-            
+            print "compute values at distributions"
+            print hx[n1], hx[1],x[1]
             f(&x[n1], &hx[n1], &hpx[n1])
             fx = hx[n1]-huzmax[0]
-            
+            print hx[n1]
             if (alu1 < (fx-alhu)):
                sampld = True
             # update while the number of points defining the hulls is lower than ns
@@ -303,34 +306,27 @@ cdef void splhull(double *u2, int* ipt, int *ilow,
              eh = logdu+logtg-eh
              if (eh < emax[0]):
                 beta[0] = xlb[0]+log(1.0+sign*expon(eh, emax[0]))/hpx[i[0]]
-                
              else:
                 beta[0] = xlb[0]+eh/hpx[i[0]]
-                
         else:
           #hpx(i) must be positive, x(ilow) is left of the mode
           beta[0] = (log(hpx[i[0]]*u2[0])+alcu[0]-hx[i[0]]+x[i[0]]*hpx[i[0]]+huzmax[0])/hpx[i[0]]
-          
+
       else:
         #sample above(j)
         eh = huz[j[0]]-huzmax[0]-alcu[0]
-        
         horiz = (fabs(hpx[i[0]]) < eps[0])
         if (horiz):
            beta[0] = z[j[0]]+(u2[0]-scum[j[0]])*expon(-eh, emax[0])
-           
         else:
             sign = fabs(hpx[i[0]])/hpx[i[0]]
             logtg = log(fabs(hpx[i[0]]))
             logdu = log(u2[0]-scum[j[0]])
             eh = logdu+logtg-eh
-            
             if (eh < emax[0]):
               beta[0] = z[j[0]]+(log(1.0+sign*expon(eh, emax[0])))/hpx[i[0]]
-              
             else:
               beta[0] = z[j[0]]+eh/hpx[i[0]]
-              
       return
 
 cdef void intersection(double *x1, double *y1, double *yp1, double *x2, double *y2,
@@ -403,6 +399,25 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
       eps  rwv[3]    : =exp(-emax) a very small number
       """
       n[0] = n[0]+1
+      #made a change here
+      print "ipt:", ipt[0]
+      print "scum: ", scum[0]
+      print "number of points defining the hulls", n[0]
+      print " values of x: " ,  x[n[0]]
+      print "index of the smallest x(i)", ilow[0] 
+      print " values of x: " ,x[ilow[0]]
+      print "z: ", z[0]
+      print "huz: ",huz[0]
+      print "huzmax: ", huzmax[0]
+      print "emax: ", emax[0]
+      print "xlb: ", xlb[0]
+      print "hulb: ", hulb[0]
+      print "xub: ", xub[0]
+      print "huub: ", huub[0]
+      print "ifault: ", ifault[0] 
+      print "eps: ", eps[0] 
+      print "alcu: ", alcu[0]
+      print "Update z,huz and ipt   "                              
 
       #update z, huz and ipt
       if (x[n[0]] < x[ilow[0]]):
@@ -412,6 +427,8 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
              ifault[0] = 5
          ipt[n[0]]=ilow[0]
          intersection(&x[n[0]], &hx[n[0]], &hpx[n[0]], &x[ilow[0]], &hx[ilow[0]], &hpx[ilow[0]], &z[n[0]], &huz[n[0]], eps, ifault)
+         print "insert x(n) below x(ilow)"
+         print "value of x, z at n :", x[n[0]], z[n[0]], huz[n[0]]
          if (ifault[0] != 0):
              return
          if (lb[0]):
@@ -421,9 +438,11 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
         i = ilow[0]
         j = i
         #find where to insert x(n)
+        print "Check: ", x[n[0]],x[i]
         while ((x[n[0]]>=x[i]) and (ipt[i] != 0)):
           j = i
           i = <int>ipt[i]
+          print "find where to insert x(n) : ", i, ilow[0]
         if (x[n[0]] >= x[i]):
            # insert above x(ihigh)
            # test for non-concavity
@@ -434,6 +453,9 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
            ipt[i] = n[0]
            ipt[n[0]] = 0
            intersection(&x[i], &hx[i], &hpx[i], &x[n[0]], &hx[n[0]], &hpx[n[0]], &z[i], &huz[i], eps, ifault)
+           print "insert x(n) above x(ihigh) " 
+           print "value of z at i :" ,i ,z[i]  
+           print "value of x at n:", n[0], x[n[0]]
            if (ifault[0] != 0):
               return
            huub[0] = hpx[n[0]]*(xub[0]-x[n[0]])+hx[n[0]]
@@ -449,20 +471,31 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
            ipt[n[0]]=i
            # insert z(j) between x(j) and x(n)
            intersection(&x[j], &hx[j], &hpx[j], &x[n[0]], &hx[n[0]], &hpx[n[0]], &z[j], &huz[j], eps, ifault)
+           print "insert z(j) between x(j) and x(n)" 
+           print j, n[0], z[j], x[j], x[n[0]]
            if (ifault[0] != 0):
               return
            #insert z(n) between x(n) and x(i)
            intersection(&x[n[0]], &hx[n[0]], &hpx[n[0]], &x[i], &hx[i], &hpx[i], &z[n[0]], &huz[n[0]], eps, ifault)
+           print "insert z(n) between x(n) and x(i)"       
+           print n[0], i , z[n[0]] , x[n[0]], x[i]          
            if (ifault[0] != 0):
               return
       #update huzmax
       j = ilow[0]
+      # made change here
+      print "Update huzmax....."
+      print "the index of the x(.) ",ipt[j] 
+      print "indexes of the highest and smallest", ihigh[0], ilow[0]
       i = <int>ipt[j]
       huzmax[0] = huz[j]
+      print "huzmax:",huzmax[0]                                                   
       while ((huz[j] < huz[i]) and (ipt[i] != 0)):
         j = i
         i = <int>ipt[i]
         huzmax[0] = max(huzmax[0], huz[j])
+      print "maximum of huz(i) : ", huzmax[0] 
+      print "value of i: ",i
       if (lb[0]):
           huzmax[0] = max(huzmax[0], hulb[0])
       if (ub[0]):
@@ -471,21 +504,30 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
       #scum receives area below exponentiated upper hull left of z(i)
       i = ilow[0]
       horiz = (fabs(hpx[ilow[0]]) < eps[0])
+      print "Update cu..."
+      print "cu:", cu[0], horiz 
       if ((not lb[0]) and (not horiz)):
+          print "NOT LOWER BOUND .."
           cu[0] = expon(huz[i]-huzmax[0], emax[0])/hpx[i]
       elif (lb[0] and horiz):
+          print "LOWER BOUND .."
           cu[0] = (z[ilow[0]]-xlb[0])*expon(hulb[0]-huzmax[0], emax[0])
       elif (lb[0] and (not horiz)):
           dh = hulb[0]-huz[i]
           if (dh > emax[0]):
+             print "dh:", dh
              cu[0] = -expon(hulb[0]-huzmax[0], emax[0])/hpx[i]
           else:
+             print "huz:", huz[i]
              cu[0] = expon(huz[i]-huzmax[0], emax[0])*(1-expon(dh, emax[0]))/hpx[i]
       else:
         cu[0] = 0
       scum[i]=cu[0]
+      print "scum:", scum[i] 
       j = i
       i = <int>ipt[i]
+      print "i:", i, ", scum:", scum[i]
+      print "the index of the x(.)" ,ipt[i]                                          
       cdef int control_count = 0
       while (ipt[i] != 0):
         if (control_count > n[0]):
@@ -516,12 +558,18 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
          else:
             cu[0] += expon(huub[0]-huzmax[0], emax[0])*(1-expon(dh, emax[0]))/hpx[i]
       scum[i]=cu[0]
+      print "INDEX i: ", i 
+      print "normalize scum at i :", scum[i]
       if (cu[0] > 0):
          alcu[0] = log(cu[0])
       #normalize scum to obtain a cumulative probability while excluding
       #unnecessary points
       i = ilow[0]
+      print "CHANGE i VALUE"
+      print "INDEX i (ilow): ", i
+      print "normalize scum at i :", scum[i]                                                           
       u = (cu[0]-scum[i])/cu[0]
+      print "normalization value :", u        
       if ((u == 1.0) and (hpx[<int>ipt[i]] > zero)):
         ilow[0] = <int>ipt[i]
         scum[i] = 0.0
@@ -529,6 +577,7 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
         scum[i] = 1.0-u
       j = i
       i = <int>ipt[i]
+      print "new value of i and ilow: ", i, ilow[0]
       while (ipt[i] != 0):
         j = i
         i = <int>ipt[i]
@@ -537,6 +586,7 @@ cdef void update(int *n, int *ilow, int *ihigh, int* ipt, double* scum, double
           ilow[0] = i
         else:
           scum[j] = 1.0 - u
+        print "ilow after all: ", i, ilow[0]
       scum[i] = 1.0
       if (ub[0]):
           huub[0] = hpx[ihigh[0]]*(xub[0]-x[ihigh[0]])+hx[ihigh[0]]
@@ -562,41 +612,23 @@ cdef void C_normal(double* u,
      ypu[0]= -u[0]      
      return
  
-def normal(double u,
-           double yu,
-           double ypu):          
-     yu = -u*u*0.5                                                               
-     ypu= -u      
-     return
-     
+
+def normal_wrapper(double x,double hx, double hpx):
+    cdef _SampleFunc S=_SampleFunc()
+    S.func = C_normal(&x,&hx,&hpx)
+    return S
+
+                 
 cdef class _SampleFunc:
        cdef void (*func)(double *, double *, double *) 
-       
-       
 def py_ars(int ns, int m, double emax,
            np.ndarray[ndim=1, dtype=np.float64_t] x,
            np.ndarray[ndim=1, dtype=np.float64_t] hx,
            np.ndarray[ndim=1, dtype=np.float64_t] hpx,
            int num,
-           f #log of the distribution
+           _SampleFunc f not None #log of the distribution
            ):
-    #Solution
-    def func_wrapper( arg1, arg2, arg3):
-        # x is a slightly opaque ctypes type
-        # first cast it to a ctypes array of known size
-        # and then create a numpy array from that
-        arg1_as_ctypes_array = (ctypes.c_double*1).from_address(ctypes.addressof(arg1.contents))
-        arg2_as_ctypes_array = (ctypes.c_double*1).from_address(ctypes.addressof(arg2.contents))
-        arg3_as_ctypes_array = (ctypes.c_double*1).from_address(ctypes.addressof(arg3.contents))
-        return f(np.asarray(arg1_as_ctypes_array),np.asarray(arg2_as_ctypes_array),np.asarray(arg3_as_ctypes_array))
-    FTYPE = ctypes.CFUNCTYPE(None, # return type
-                             ctypes.POINTER(ctypes.c_double),
-                             ctypes.POINTER(ctypes.c_double),
-                             ctypes.POINTER(ctypes.c_double))
-    f_ctypes = FTYPE(func_wrapper) # convert Python callable to ctypes function pointer
 
-    # a rather nasty line to convert to a C function pointer
-    cdef func_t f_ptr = (<func_t*><size_t>ctypes.addressof(f_ctypes))[0]    
     cdef np.ndarray[ndim=1, dtype=np.float64_t] rwv, sp
     cdef np.ndarray[ndim=1, dtype=np.int64_t] iwv
     # initializing arrays
@@ -609,7 +641,7 @@ def py_ars(int ns, int m, double emax,
     cdef int lb=0
     cdef int ub=0
     cdef int ifault = 999
-    cdef double beta 
+    cdef double beta = 0.
 
     initial(&ns, &m, &emax,
             &x[0], # passing array by reference
@@ -624,28 +656,21 @@ def py_ars(int ns, int m, double emax,
             &rwv[0] # passing array by reference
             )
 
-    #cdef int j
-    #for j from 0 <= j <(ns*6+15):
-    #    print rwv[j]
     cdef int i
+    
     for i from 0 <= i <num:
-        beta = 0.
+        
         sample(
                <int *>(&iwv[0]), # passing array by reference
                &rwv[0], # passing array by reference
-               f_ptr,
+               f.func,
                &beta, # passing double variable by reference
                &ifault, # passing integer variable by reference
                )
         
         sp[i] = beta
+
     return sp   
 
 
-def run(int ns, int m, double emax,
-           np.ndarray[ndim=1, dtype=np.float64_t] x,
-           np.ndarray[ndim=1, dtype=np.float64_t] hx,
-           np.ndarray[ndim=1, dtype=np.float64_t] hpx,
-           int num
-           ):
-    return py_ars(ns, m, emax, x, hx, hpx, num, normal)  
+
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ars import *
 import ctypes
+import pylab as plt
 m = 3
 ns = 100
 emax = 64.
@@ -22,7 +23,10 @@ print x
 print hx
 print hpx
 
-num = 20
-sp=py_ars(ns, m, emax, x, hx, hpx, num, normal_wrapper)
+num = 10000
+sp=run(ns, m, emax, x, hx, hpx, num)
 
-print(sp)
+print sp
+
+n, bins, patches = plt.hist(x, 50, normed=1, histtype='stepfilled')
+#plt.show()
